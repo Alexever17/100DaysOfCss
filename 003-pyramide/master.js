dd5065f9a5e4d5346a1f46975100010c55ad7192
alexever17@gmail.com <alexever17@gmail.com>
2018-01-03 01:03:36 +0100

    third project version 1

---
+++ 003-pyramide/master.js
@@ -0,0 +1,4 @@
+var frame = document.getElementsByClassName("frame")[0];
+var h = document.getElementsByClassName("h1")[0];
+frame.style.left = ((document.body.offsetWidth - frame.offsetWidth) / 2) + "px";
+h.style.left = ((document.body.offsetWidth - h.offsetWidth) / 2) + "px";
